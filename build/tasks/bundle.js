0b71601507f5e4422ad74904e80f4ad475ebfa4a
Ats Uiboupin <ats.uiboupin@iglu.ee>
2016-01-05 20:28:14 +0200

    allow bundling Aurelia code without application code

--- build/tasks/bundle.js
+++ build/tasks/bundle.js
@@ -10,9 +10,36 @@ var config = {
 };
 
 gulp.task('bundle', ['unbundle', 'build'], function() {
-  return bundler.bundle(config);
+	return bundle(config, null);
 });
 
 gulp.task('unbundle', function() {
-  return bundler.unbundle(config);
+	return unbundle(config, null);
 });
+
+gulp.task('bundleAurelia', ['unbundleAurelia', 'build'], function() {
+	return bundle(config, true);
+});
+
+gulp.task('unbundleAurelia', function() {
+	return unbundle(config, true);
+});
+
+function bundle(config, onlyAurelia) {
+	return bundler.bundle(getBundleConfig(config, onlyAurelia));
+}
+
+function unbundle(config, onlyAurelia) {
+	return bundler.unbundle(getBundleConfig(config, onlyAurelia));
+}
+
+function getBundleConfig(config, onlyAurelia) {
+	if(onlyAurelia == null) {
+		// do nothing
+	} else if (onlyAurelia) {
+		delete config.bundles["dist/bundled-app"];
+	} else if (onlyAurelia) {
+		delete config.bundles["dist/bundled-aurelia"];
+	}
+	return config
+}
